81b56187b56afeec5407d1f19c62a41d51314e0e
Hanialderbane-quran <xd.mod.xd2021@gmail.com>
2026-07-20 23:15:00 +0300

    Create youtube_engine.py

---
+++ generator/youtube_engine.py
@@ -0,0 +1,17 @@
+"""
+Quran AI Publisher
+YouTube Engine
+Version 1.0
+"""
+
+
+def upload(video_path, seo):
+
+    print()
+    print("========== YOUTUBE ==========")
+    print("Video :", video_path)
+    print("Title :", seo["title"])
+    print("Status: READY")
+    print("=============================")
+
+    return True
