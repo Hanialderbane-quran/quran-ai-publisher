37392b61e83d9f120e28d2f1562f68b2f0afe4f1
Hanialderbane-quran <xd.mod.xd2021@gmail.com>
2026-07-22 10:41:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,44 @@ from datetime import datetime
 
 from generator.tasks import run_tasks
 from generator.safety import run_safety_check
+from generator.assets_manager import check_assets
 from generator.brain import think
+from generator.quality_engine import validate
 from generator.report_engine import create_report
 from generator.video_engine import build_video
-from generator.quality_engine import validate
+from generator.youtube_engine import upload
+from generator.state_manager import (
+    increase_video_counter,
+    update_last_publish
+)
 
 
 def load_config():
+
     with open("config.json", "r", encoding="utf-8") as file:
+
         return json.load(file)
 
 
 def start():
 
-    print("========== Quran AI Publisher ==========\n")
+    print("========== Quran AI Publisher ==========")
 
     run_tasks()
 
     if not run_safety_check():
         return
 
+    if not check_assets():
+        return
+
     config = load_config()
 
-    print("Channel :", config["channel_name"])
-    print("Time    :", datetime.now())
     print()
+
+    print("Channel :", config["channel_name"])
+
+    print("Time :", datetime.now())
 
     result = think()
 
@@ -35,6 +48,7 @@ def start():
         return
 
     verse = result["verse"]
+
     seo = result["seo"]
 
     if not validate(verse, seo):
@@ -42,11 +56,23 @@ def start():
 
     create_report(verse, seo)
 
-    build_video(verse, seo)
+    video = build_video(verse, seo)
+
+    upload(video, seo)
+
+    update_last_publish(
+
+        verse["surah"],
+
+        verse["ayah"]
+
+    )
+
+    increase_video_counter()
 
     print()
-    print("Publisher finished successfully.")
-    print("Ready for YouTube upload.")
+
+    print("Daily Publisher Finished Successfully")
 
 
 if __name__ == "__main__":
